8a454f89a296d3994910339515a39d4bff1544aa
IdoYe <idoye@campus.technion.ac.il>
2020-04-01 13:47:45 +0300

    adding counting solve problem

--- HW_1/Question_2_Counting.py
+++ HW_1/Question_2_Counting.py
@@ -3,14 +3,13 @@ import numpy as np
 N = 12
 X = 800
 
-intermediate_results = np.zeros((N + 1, X + 1))
+interRes = np.zeros((N + 1, X + 1))
 
 for x in range(1, X + 1):
-    intermediate_results[1, x] = 1
+    interRes[1, x] = 1
 
-for x in range(1, X + 1):
-    for n in range(1, N + 1):
-        for i in range(1, x):
-            intermediate_results[n, x] += intermediate_results[n - 1, x - i]
+for n in range(2, N + 1):
+    for x in range(1, X + 1):
+        interRes[n, x] = interRes[n, x - 1] + interRes[n - 1, x - 1]
 
-print(int(intermediate_results[12, 800]))
+print(int(interRes[12, 800]))
