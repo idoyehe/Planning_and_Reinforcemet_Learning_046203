dfcdd077574364a3c4478ba4e0f130cbb505a6dd
IdoYe <idoye@campus.technion.ac.il>
2020-05-30 15:29:55 +0300

    fix q learning

--- Wet_2/job_scheduling_q_learning.py
+++ Wet_2/job_scheduling_q_learning.py
@@ -56,50 +56,45 @@ def q_learning(epsilon, num_iteration, alpha_n_method):
             Q_S_A[state][-1] = 0
     epsilon_greedy_policy = createEpsilonGreedyPolicy(Q_S_A, epsilon)
 
-    visited_in_state_counter = defaultdict(int)
-    current_state = INIT_STATE
-    visited_in_state_counter[current_state] += 1
+    visited_in_state_counter = np.zeros(shape=(STATE_SPACE_SIZE, ACTION_SPACE_SIZE+1))
 
     max_norm_delta = list()
     s0_delta = list()
 
     # For every episode
     for ith_episode in range(num_iteration):
-        # get probabilities of all actions from current state
-        action_probabilities = epsilon_greedy_policy(current_state)
+        for current_state, current_state_index in STATE_SPACE_INDEX_DICT.items():
+            # get probabilities of all actions from current state
+            action_probabilities = epsilon_greedy_policy(current_state)
 
-        # choose action according to the probability distribution
-        action = np.random.choice(list(action_probabilities.keys()), p=list(action_probabilities.values()))
+            # choose action according to the probability distribution
+            action = np.random.choice(list(action_probabilities.keys()), p=list(action_probabilities.values()))
 
-        # take action and get reward, transit to next state
-        current_cost_state, next_state, done = simulator(current_state, action)
-        visited_in_state_counter[next_state] += 1
+            visited_in_state_counter[current_state_index][action] += 1
 
-        next_state_index = STATE_SPACE_INDEX_DICT[next_state]
+            # take action and get reward, transit to next state
+            current_cost_state, next_state, done = simulator(current_state, action)
 
-        best_next_action = int(createGreedyPolicy(Q_S_A)[next_state_index])
-        td_target = current_cost_state + Q_S_A[next_state][best_next_action]
-        td_delta = td_target - Q_S_A[current_state][action]
+            next_state_index = STATE_SPACE_INDEX_DICT[next_state]
 
-        if alpha_n_method == 1:
-            alpha_n = 1 / visited_in_state_counter[current_state]
-        elif alpha_n_method == 2:
-            alpha_n = 0.01
-        else:
-            alpha_n = 10 / (100 + visited_in_state_counter[current_state])
+            best_next_action = int(createGreedyPolicy(Q_S_A)[next_state_index])
 
-        Q_S_A[current_state][action] += alpha_n * td_delta
+            td_target = current_cost_state + Q_S_A[next_state][best_next_action]
+            td_delta = td_target - Q_S_A[current_state][action]
+
+            if alpha_n_method == 1:
+                alpha_n = 1 / visited_in_state_counter[current_state_index][action]
+            elif alpha_n_method == 2:
+                alpha_n = 0.01
+            else:
+                alpha_n = 10 / (100 + visited_in_state_counter[current_state_index][action])
+
+            Q_S_A[current_state][action] += alpha_n * td_delta
+
         current_greedy_policy_value = calculate_value_function_by_policy(createGreedyPolicy(Q_S_A))
-
         max_norm_delta.append(np.max(np.abs(optimal_value_function - current_greedy_policy_value)))
         s0_delta.append(np.abs(optimal_value_function[0] - Q_S_A[INIT_STATE][int(createGreedyPolicy(Q_S_A)[0])])[0])
 
-        # done is True if episode terminated
-        if done:
-            current_state = INIT_STATE
-            visited_in_state_counter[current_state] += 1
-        else:
-            current_state = next_state
     return max_norm_delta, s0_delta
 
 
@@ -116,7 +111,7 @@ def __plot_deltas(max_norm_delta, s0_delta):
 
 
 if __name__ == "__main__":
-    num_iteration = 100000
+    num_iteration = 10000
     # alpha n is 1/ number of visits
     max_norm_delta, s0_delta = q_learning(epsilon=0.1, num_iteration=num_iteration, alpha_n_method=1)
     __plot_deltas(max_norm_delta, s0_delta)
@@ -125,10 +120,10 @@ if __name__ == "__main__":
     max_norm_delta, s0_delta = q_learning(epsilon=0.1, num_iteration=num_iteration, alpha_n_method=2)
     __plot_deltas(max_norm_delta, s0_delta)
 
-    # # alpha n is 10 / (100 + number of visits)
-    # max_norm_delta, s0_delta = q_learning(epsilon=0.1, num_iteration=num_iteration, alpha_n_method=3)
-    # __plot_deltas(max_norm_delta, s0_delta)
-    #
-    # # alpha n is 10 / (100 + number of visits) with epsilon 0.01
-    # max_norm_delta, s0_delta = q_learning(epsilon=0.01, num_iteration=num_iteration, alpha_n_method=3)
-    # __plot_deltas(max_norm_delta, s0_delta)
+    # alpha n is 10 / (100 + number of visits)
+    max_norm_delta, s0_delta = q_learning(epsilon=0.1, num_iteration=num_iteration, alpha_n_method=3)
+    __plot_deltas(max_norm_delta, s0_delta)
+
+    # alpha n is 10 / (100 + number of visits) with epsilon 0.01
+    max_norm_delta, s0_delta = q_learning(epsilon=0.01, num_iteration=num_iteration, alpha_n_method=3)
+    __plot_deltas(max_norm_delta, s0_delta)
